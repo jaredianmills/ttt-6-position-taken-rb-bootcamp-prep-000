940364bec20990eec902ad59dda55373f8d6681a
jaredianmills <jaredianmills@gmail.com>
2018-05-12 18:33:40 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,3 +2,7 @@ def position_taken?(board, index)
   board[index] == "" || board[index] == " " || board[index] == nil
   board[index] == "X" || board[index] == "O"
 end
+
+board = [" ", "X", " "]
+
+position_taken?()
